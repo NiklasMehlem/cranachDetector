086626cbb06dd4f40f4e07b0da57fe628fc75815
Niklas Mehlem <niklas.mehlem@smail.th-koeln.de>
2025-05-18 21:05:23 +0200

    + Ordner- und Dateistruktur für das kommende Package hinzugefügt ; + Lizenzen und Dependencys des Packages hinzugefügt

---
+++ src/cranach_detector/__init__.py
@@ -0,0 +1,4 @@
+from .detector import CranachDetector
+
+__all__ = ["CranachDetector"]
+__version__ = "0.1.0"
---
+++ src/cranach_detector/detector.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# Dieses Modul verwendet:
+# - Dlib CNN (Boost Software License 1.0)
+# - MTCNN (MIT License)
+# - InsightFace Code (MIT License)
+# - RetinaFace (MIT License)
+# =============================================================================
+
+### imports
+import dlib
+import matplotlib.pyplot as plt
+from insightface.app import FaceAnalysis
+from mtcnn import MTCNN
+from pathlib import Path
+
+class CranachDetector:
+    print("Hello TODO")
